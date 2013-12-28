a6f60f12f91a9749e1f68a1798d28fa68bf62362
Micah Brandon <brandon@pakkun.netsville.com>
2013-12-27 21:53:53 -0500

    Fixing my version of D30.java to match upstream

--- BitcoinMonitor/src/main/java/pl/d30/bitcoin/D30.java
+++ BitcoinMonitor/src/main/java/pl/d30/bitcoin/D30.java
@@ -14,35 +14,6 @@ public class D30 {
     public static final String LOG = "DashCoin";
 
     // configuration files
-<<<<<<< HEAD
-    public static final String PREF_FILE_BTC = "btc";
-    public static final String PREF_FILE_LTC = "ltc";
-
-
-    //configuration indexes
-    public static final String IDX_SOURCE = "source";
-    public static final String IDX_CURRENCY = "currency";
-    public static final String IDX_AMOUNT= "amount";
-    public static final String IDX_EXPERIMENTAL = "experimental";
-
-
-    //values
-    public static final int BTC = 0;
-    public static final int LTC = 1;
-
-    public static final int MTGOX = 0;
-    public static final int BITSTAMP = 1;
-    public static final int BTCE = 2;
-    public static final int CAMPBX = 3;
-
-    public static final String USD = "USD";
-    public static final String EUR = "EUR";
-
-
-    // DEFAULTS
-    public static final String DEF_CURRENCY = USD;
-
-=======
     public static final String PREF_FILE_BTC    = "btc";
     public static final String PREF_FILE_LTC    = "ltc";
     public static final String PREF_FILE_NOTIF  = "notif";
@@ -76,7 +47,6 @@ public class D30 {
 
 
     // Json helper class:
->>>>>>> upstream/master
     public static class Json {
 
         public static JsonObject getObject(JsonObject j, String n) {
